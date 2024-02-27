3eba8274961e6d8c8ef5e59b3c645083909215ca
Ilefkristou <159954471+Ilefkristou@users.noreply.github.com>
2024-02-27 18:27:45 +0100

    Create RMIServer.java

---
+++ RMIServer.java
@@ -0,0 +1,17 @@
+import java.rmi.Naming;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+
+public class RMIServer {
+    public static void main(String[] args) {
+        try {
+            LocateRegistry.createRegistry(1099); // Utilisation du port par défaut
+            RemoteInterface remoteObj = new RemoteImplementation();
+            Naming.rebind("RemoteObject", remoteObj);
+            System.out.println("Server is running...");
+        } catch (Exception e) {
+            System.err.println("Server exception: " + e.toString());
+            e.printStackTrace();
+        }
+    }
+}
